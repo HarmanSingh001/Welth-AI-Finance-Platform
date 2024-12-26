5447188d00963cfb21bfecb4bbe984528f4ad6ab
Harman Singh <harmansingh2670@gmail.com>
2024-12-27 00:15:54 +0530

    design header,footer and import  env files

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,8 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
     return (
@@ -15,14 +17,45 @@ const Header = () => {
                         className="h-12 w-auto object-contain"
                     />
                 </Link>
-            </nav>
+                <div className="flex items-center space-x-4">
+                    <SignedIn>
+                        <Link
+                            href={"/dashboard"}
+                            className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+                        >
+                            <Button variant="outline">
+                                <LayoutDashboard size={18} />
+                                <span className="hidden md:inline">
+                                    Dashboard
+                                </span>
+                            </Button>
+                        </Link>
+                        <Link href={"/transaction/create"}>
+                            <Button className="flex items-center gap-2">
+                                <PenBox size={18} />
+                                <span className="hidden md:inline">
+                                    Add Transaction
+                                </span>
+                            </Button>
+                        </Link>
+                    </SignedIn>
 
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/dashboard">
+                            <Button variant="outline">Login</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-10 h-10",
+                                },
+                            }}
+                        />
+                    </SignedIn>
+                </div>
+            </nav>
         </div>
     );
 };
