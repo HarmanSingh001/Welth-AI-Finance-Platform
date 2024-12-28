8d3121d0aeb4262ca87f9bd23cd02bb72cffd7b1
Harman Singh <harmansingh2670@gmail.com>
2024-12-29 00:22:52 +0530

    new updates

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Link from "next/link";
-import { Button } from "./ui/button";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const HeroSection = () => {
-    const imageRef = useRef();
+    const imageRef = useRef(null);
 
     useEffect(() => {
         const imageElement = imageRef.current;
@@ -23,15 +23,14 @@ const HeroSection = () => {
         };
 
         window.addEventListener("scroll", handleScroll);
-
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
     return (
-        <div className="pb-20 px-4">
+        <section className="pt-40 pb-20 px-4">
             <div className="container mx-auto text-center">
                 <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
-                    Manage Your Finances <br /> With Intelligence
+                    Manage Your Finances <br /> with Intelligence
                 </h1>
                 <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
                     An AI-powered financial management platform that helps you
@@ -44,13 +43,13 @@ const HeroSection = () => {
                             Get Started
                         </Button>
                     </Link>
-                    <Link href="https://github.com/HarmanSingh001/Welth-AI-Finance-Platform">
+                    <Link href="https://www.youtube.com/roadsidecoder">
                         <Button size="lg" variant="outline" className="px-8">
                             Watch Demo
                         </Button>
                     </Link>
                 </div>
-                <div className="hero-image-wrapper">
+                <div className="hero-image-wrapper mt-5 md:mt-0">
                     <div ref={imageRef} className="hero-image">
                         <Image
                             src="/banner.jpeg"
@@ -63,7 +62,7 @@ const HeroSection = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
